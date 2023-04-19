c16f8d9bc4bffcdcc28ccece5d98b8741317fb9e
janafojtova <124291649+janafojtova@users.noreply.github.com>
2023-04-19 14:33:28 +0200

    first commit

---
+++ cviceni-rodna-cisla/script.js
@@ -0,0 +1,107 @@
+console.log('funguju!');
+
+const rodneCisloOdUzivatele = prompt('Zadejte rodné číslo bez lomítka');
+const digits = ['0','1','2','3','4','5','6','7','8','9'];
+
+const isDigit = (cislo) =>{
+    if(digits.includes(cislo)){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(isDigit('1'));
+console.log(isDigit('33'));
+
+
+
+//document.write(pole.toString());
+const rodneCisloOdUzivatelePole = rodneCisloOdUzivatele.split("");
+const logInvalidCharacter = (rodneCisloOdUzivatelePole)=>{
+    rodneCisloOdUzivatelePole.forEach( znak=> {
+        if(isDigit(znak)){
+        }else{
+            console.log(znak);
+        }
+    });
+}
+console.log(logInvalidCharacter(rodneCisloOdUzivatelePole));
+
+const validateCharacters =(rodneCisloOdUzivatelePole)=>{
+    const result =[];
+    
+
+    rodneCisloOdUzivatelePole.forEach( znak=> {
+        let jeCislo = isDigit(znak);
+        let novyObjekt = `{ char: '${znak}', digit: '${jeCislo}' }` 
+     
+         result.push(novyObjekt)
+    });
+    console.log(result)
+}
+console.log(validateCharacters(rodneCisloOdUzivatelePole));
+
+const formElm = document.querySelector('#kontrolaRodnehoCisla');
+formElm.addEventListener('submit', (event)=>{
+     event.preventDefault();
+     const rodneCislo = document.querySelector('#rodneCislo').value;
+     const vyhodnoceni = document.querySelector('#vyhodnoceni');
+      
+     if(checkBirthID(rodneCislo)==='ok'){
+        vyhodnoceni.innerHTML= 'V pořádku'
+     }else{
+        vyhodnoceni.innerHTML= 'V rodném číslu je chyba'
+     };
+    
+     const kontrolaZnaku = document.querySelector('#vypisRodnehoCisla')
+     const rodneCisloZFormulare = rodneCislo.split("");
+     const pole = validateCharacters(rodneCisloZFormulare);
+     console.log(pole)
+    
+    // pole.forEach( znak=> {
+    //  kontrolaZnaku.innerHTML+=`<div>${znak}</div>`;
+    //  if(isDigit(znak)){            
+    //      kontrolaZnaku.classList.add('platnaCislice')
+    //  } else{
+    //      kontrolaZnaku.classList.add('neplatnaCislice')
+    //  } 
+    // })
+})
+
+
+
+
+
+const checkBirthID =(rodneCisloOdUzivatele)=>{
+    const delka = rodneCisloOdUzivatele.length;
+    if(delka===10){
+        console.log('Zadané rodné číslo má přesně deset znaků.');
+    }else{
+        console.log('Uživatel zadal rodné číslo neplatné délky');
+        return 'invalidLength';
+    }
+
+    const jeCislo =  Number.isInteger(Number(rodneCisloOdUzivatele));
+    if(jeCislo===true){
+        console.log('Rodné číslo je celé číslo');
+    }else{
+        console.log('Rodné číslo obsahuje nepovolené znaky');
+        return 'notANumber';
+    }
+
+    if(Number(rodneCisloOdUzivatele)%11===0 ){
+        console.log('Rodné číslo je dělitelné 11');
+    }else{
+        console.log('Rodné číslo není dělitelné 11');
+        return 'failedChecksum';
+    }
+
+    if(delka===10 && Number(rodneCisloOdUzivatele)%11===0 && jeCislo===true){
+        console.log('Zadané rodné číslo je platné');
+        return 'ok'
+    }else{
+        console.log('Uživatelem zadané rodné číslo není platné');
+    }
+}
+// 
+
